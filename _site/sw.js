806edf74ae4f7eab3571235bba106577e4d8149e
harshoverseer <underseer@gmail.com>
2026-04-25 18:59:08 +1000

    fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250816.210923';
+const cacheName = 'chirpy-20260425.185517';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
